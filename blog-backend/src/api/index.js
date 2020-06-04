d2710088bf42e4f81e454ac5613c70a209675ea8
znzmektm2 <tingcber2@naver.com>
2020-06-05 06:17:23 +0900

    add: esm으로 ES 모듈 import/export 문법 사용하기, 데이터베이스의 스키마와 모델 생성

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,13 +1,9 @@
-const Router = require('koa-router');
-const posts = require('./posts');
+import Router from 'koa-router';
+import posts from './posts';
 
 const api = new Router();
-
-// api.get('/test', ctx => {
-//   ctx.body = 'test 성공';
-// });
 
 api.use('/posts', posts.routes());
 
 // 라우터를 내보냅니다
-module.exports = api;
+export default api;
