62846260c25cd79ebbb7a6401bb5bd87f6e95bfa
znzmektm2 <tingcber2@naver.com>
2020-06-05 03:28:29 +0900

    add: 프로젝트 구조 생성, Koa 기본 사용법, nodemon 사용하기

---
+++ blog-backend/src/index.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(async(ctx, next) => {
+  console.log(ctx.url);
+  console.log(1);
+  if(ctx.query.authorized !== '1') {
+    ctx.status = 401; //Unathorized
+    return;
+  }
+  // next().then(() => {
+  //   console.log('END');
+  // });
+  await next();
+  console.log('END');
+});
+
+app.use((ctx,next) => {
+  console.log(2);
+  next();
+});
+  
+app.use(ctx => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
