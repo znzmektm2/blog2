2252ef74def08f75e7efafc7e742d663d16f8299
znzmektm2 <tingcber2@naver.com>
2020-06-05 03:50:09 +0900

    add: koa-router 사용하기

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,29 +1,51 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async(ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if(ctx.query.authorized !== '1') {
-    ctx.status = 401; //Unathorized
-    return;
-  }
-  // next().then(() => {
-  //   console.log('END');
-  // });
-  await next();
-  console.log('END');
+// 라우터 설정
+router.get('/', ctx => {
+  ctx.body = '홈';
 });
 
-app.use((ctx,next) => {
-  console.log(2);
-  next();
+router.get('/about/:name?', ctx => {
+  const { name } = ctx.params;
+  // name의 조내 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : '소개';
 });
+
+router.get('/posts', ctx => {
+  const { id } = ctx.query;
+  // id의 존재 유무에 따라 다른 결과 출력
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+// app.use(async(ctx, next) => {
+//   console.log(ctx.url);
+//   console.log(1);
+//   if(ctx.query.authorized !== '1') {
+//     ctx.status = 401; //Unathorized
+//     return;
+//   }
+//   // next().then(() => {
+//   //   console.log('END');
+//   // });
+//   await next();
+//   console.log('END');
+// });
+
+// app.use((ctx,next) => {
+//   console.log(2);
+//   next();
+// });
   
-app.use(ctx => {
-  ctx.body = 'hello world';
-});
+// app.use(ctx => {
+//   ctx.body = 'hello world';
+// });
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
