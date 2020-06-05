68b45096518656e0107768fe4e98d6140afc696f
znzmektm2 <tingcber2@naver.com>
2020-06-06 05:39:47 +0900

    add: 프론트엔드 프로젝트 작업 환경 준비하기

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
   );
 };
 
