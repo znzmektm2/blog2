f5e42bb0f04bb1f389e99722df833e1e7b183c0e
znzmektm2 <tingcber2@naver.com>
2020-06-10 14:30:22 +0900

    add: 글쓰기 기능 구현

---
+++ blog-frontend/src/modules/write.js
@@ -0,0 +1,65 @@
+import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/posts';
+import { takeLatest } from 'redux-saga/effects';
+
+const INITIALIZE = 'write/INITIAL';
+const CHANG_FIELD = 'write/CHANGE_FIELD';
+const [
+  WRITE_POST,
+  WRITE_POST_SUCCESS,
+  WRITE_POST_FAILURE,
+] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
+
+export const initialize = createAction(INITIALIZE);
+export const changeField = createAction(CHANG_FIELD, ({ key, value }) => ({
+  key,
+  value,
+}));
+export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
+  title,
+  body,
+  tags,
+}));
+
+// 사가 생성
+const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+export function* writeSaga() {
+  yield takeLatest(WRITE_POST, writePostSaga);
+}
+
+const initialState = {
+  title: '',
+  body: '',
+  tags: [],
+  post: null,
+  postError: null,
+};
+
+const write = handleActions(
+  {
+    [INITIALIZE]: (state) => initialState, // initialState를 넣으면 초기 상태로 바뀜
+    [CHANG_FIELD]: (state, { payload: { key, value } }) => ({
+      ...state,
+      [key]: value, // 특정 key 값을 업데이트
+    }),
+    [WRITE_POST]: (state) => ({
+      ...state,
+      post: null,
+      postError: null,
+    }),
+    [WRITE_POST_SUCCESS]: (state, { payload: post }) => ({
+      ...state,
+      post,
+    }),
+    [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
+      ...state,
+      postError,
+    }),
+  },
+  initialState,
+);
+
+export default write;
