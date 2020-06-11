1c487e687354959d408b0d21d1517327f95f9433
znzmektm2 <tingcber2@naver.com>
2020-06-11 19:53:39 +0900

    add: 포스트 읽기 페이지 구현하기

--- blog-frontend/src/lib/api/posts.js
+++ blog-frontend/src/lib/api/posts.js
@@ -1,4 +1,7 @@
 import client from './client';
+import { cleanup } from '@testing-library/react';
 
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
+
+export const readPost = (id) => client.get(`/api/posts/${id}`);
